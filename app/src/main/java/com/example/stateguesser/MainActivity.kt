d8f128f9408da09d53ba6a1f3fbd37cbd1caa9f4
Clayson Fellner <cmfel217@learner.dvusd.org>
2021-12-07 11:21:46 -0700

    Changed description to "Hi'

--- app/src/main/java/com/example/stateguesser/MainActivity.kt
+++ app/src/main/java/com/example/stateguesser/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //How does this work
+        //Hi
     }
 }
