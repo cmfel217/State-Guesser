5b221f07713196b88bc720d7126ce019c1831e48
Clayson Fellner <cmfel217@learner.dvusd.org>
2021-12-08 10:56:05 -0700

    Set on click listener for image view and created variable called stateImage

--- app/src/main/java/com/example/stateguesser/MainActivity.kt
+++ app/src/main/java/com/example/stateguesser/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.stateguesser
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Hi
+
+        var stateImage = findViewById<ImageView>(R.id.imageView)
+        stateImage.setOnClickListener{
+        
+        }
     }
 }
